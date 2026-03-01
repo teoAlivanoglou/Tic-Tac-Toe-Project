943bbed858669229df97737a9769b3e9609fe0d1
teoAlivanoglou <teoalivanoglou@gmail.com>
2026-03-02 00:36:45 +0200

    feat: Δημιουργία βασικού κορμού στο src μe σχόλια docstrings

---
+++ src/main.py
@@ -0,0 +1,18 @@
+"""
+Κύριο αρχείο εκκίνησης της εφαρμογής (Entry point).
+Εδώ γίνεται η αρχικοποίηση του παιχνιδιού και της γραφικής διεπαφής.
+"""
+
+from window import TicTacToeWindow
+from game import Game
+from player import HumanPlayer, RandomBot
+
+def main():
+    """
+    Κύρια συνάρτηση που εκκινεί την εφαρμογή.
+    Δημιουργεί αντικείμενα για το παιχνίδι, τους παίκτες και το παράθυρο.
+    """
+    pass
+
+if __name__ == "__main__":
+    pass
